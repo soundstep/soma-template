6ca40a6c99d324752c486087ad5627238762ee03
Romuald Quantin <romu@soundstep.com>
2012-10-22 18:24:00 +0100

    interpolation test added

---
+++ Guardfile
@@ -0,0 +1,6 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'livereload', :apply_js_live => false, :apply_css_live => true do
+  watch(%r{lab/.+\.(css|js|html)$})
+end
---
+++ grunt.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+//		pkg:'<json:package.json>',
+//		meta:{
+//			version:'<%=pkg.version%>',
+//			banner:'/*! soma.js - v<%= meta.version %> - ' +
+//				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+//				'* http://www.soundstep.com/\n' +
+//				'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+//				'Soundstep */'
+//		},
+//		concat: {
+//			dist: {
+//				src: [
+//					'libs/infuse.js',
+//					'libs/soma-events.js',
+//					'src/prefix.js',
+//					'src/utils.js',
+//					'src/core.js',
+//					'src/suffix.js'
+//				],
+//				dest: 'build/soma.js'
+//			}
+//		},
+//		min:{
+//			dest:{
+//				src:['<banner:meta.banner>', '<config:concat.dist.dest>'],
+//				dest:'build/soma-v<%= meta.version %>.min.js'
+//			}
+//		},
+//		uglify:{
+//			mangle:{
+//				except:["scope"]
+//			}
+//		},
+//		watch:{
+//			scripts:{
+//				files:[
+//					'libs/*.js',
+//					'src/*.js',
+//					'grunt.js'
+//				],
+//				tasks:'concat min'
+//			}
+//		}
+	});
+
+	grunt.registerTask('default', 'concat min');
+}
