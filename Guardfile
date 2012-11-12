8b1cee73bbda1b004014d3015b2729a250692217
Romuald Quantin <romu@soundstep.com>
2012-11-12 10:54:36 +0000

    pygment test

---
+++ Guardfile
@@ -0,0 +1,7 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'livereload', :apply_js_live => false, :apply_css_live => true do
+  watch(%r{index.html})
+  watch(%r{assets/.+\.(css|js|html)$})
+end
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+code = 'print "Hello World"'
+print highlight(code, PythonLexer(), HtmlFormatter())
