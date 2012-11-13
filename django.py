fd76f569f662e061573970af9983c0ae91d9fb15
Romuald Quantin <romu@soundstep.com>
2012-11-13 18:10:46 +0000

    site update

--- django.py
+++ django.py
@@ -3,19 +3,23 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<div id="target">
-  <p>{{tweets.results.length}} tweets retrieved in {{tweets.completed_in}} seconds.</p>
-</div>
-<script>
-  var template = soma.template.create(document.getElementById('target'));
-  $.ajax({
-    type:'GET',
-	url: 'http://search.twitter.com/search.json?q=template',
-	  dataType:'jsonp',
-	  success:function (data) {
-	    template.scope.tweets = data;
-	    template.render();
-	  }
-  });
+  <p class="bold {{styles.color}}">
+    I am {{utils.makeOlder(person.age, "years old")}}.
+  </p>
+  </div>
+  <script>
+  template.scope.styles = {
+    color: "color"
+  };
+  template.scope.person = {
+    age: 21
+  };
+  template.scope.utils = {
+    makeOlder: function(age, type) {
+      return (age*2) + " " + type;
+    }
+  };
+  template.render();
 </script>
 """
 
