cdaf3a63195b703c1619a4c847c45affb3152514
Romuald Quantin <romu@soundstep.com>
2012-11-13 00:35:43 +0000

    site update

--- test.py
+++
@@ -1,9 +0,0 @@
-from pygments import highlight
-from pygments.lexers import JavascriptLexer
-from pygments.formatters import HtmlFormatter
-
-code = 'function HelloWorld() {\n\
-	alert("hello");\n\
-}'
-
-print highlight(code, JavascriptLexer(), HtmlFormatter())
