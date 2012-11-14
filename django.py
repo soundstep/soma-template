eac988d4977bc0b168f3df46e908e9ea89be7d07
Romuald Quantin <romu@soundstep.com>
2012-11-14 03:29:40 +0000

    site update

--- django.py
+++ django.py
@@ -2,24 +2,15 @@ from pygments import highlight
 from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
-code = """<div id="target">
-  <p class="bold {{styles.color}}">
-    I am {{utils.makeOlder(person.age, "years old")}}.
-  </p>
-  </div>
-  <script>
-  template.scope.styles = {
-    color: "color"
-  };
-  template.scope.person = {
-    age: 21
-  };
-  template.scope.utils = {
-    makeOlder: function(age, type) {
-      return (age*2) + " " + type;
-    }
-  };
-  template.render();
+code = """<div id="target"><div class="{{color}}">{{content}}</div></div>
+<script>
+  template.scope.color = "color-red";
+  template.scope.content = "This paragraph is not red";
+  template.watch(template.element.firstChild, function(oldValue, newValue, pattern, scope, node, attribute) {
+    if (attribute) return "color-blue";
+    else return newValue + " and has been watched as well!";
+  });
+template.render();
 </script>
 """
 
