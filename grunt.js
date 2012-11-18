d9e841cf51d09ce2f1c2bf3f97f5fcf273271421
Romuald Quantin <romu@soundstep.com>
2012-11-18 01:47:07 +0000

    site update

--- grunt.js
+++ grunt.js
@@ -5,16 +5,32 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg:'<json:package.json>',
 		shell: {
+			delete_build_folder: {
+				command: 'rm -r build/',
+				stdout: true
+			},
 			update_build_from_master: {
 				command: 'git checkout master build',
+				stdout: true
+			},
+			delete_demos_folder: {
+				command: 'rm -r demos/',
 				stdout: true
 			},
 			update_demos_from_master: {
 				command: 'git checkout master demos',
 				stdout: true
 			},
+			delete_src_folder: {
+				command: 'rm -r src/',
+				stdout: true
+			},
 			update_src_from_master: {
 				command: 'git checkout master src',
+				stdout: true
+			},
+			delete_tests_folder: {
+				command: 'rm -r tests/',
 				stdout: true
 			},
 			update_tests_from_master: {
