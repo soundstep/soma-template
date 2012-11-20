042415ed7521474a5e1d3f73599598127f1e6507
Romuald Quantin <romu@soundstep.com>
2012-11-20 03:33:00 +0000

    site update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,36 @@
 ;(function(ns, undefined) {
 
+	var clicked = false;
+
     $('nav > ul > li').click(function() {
-	    $('nav > ul > li > ul').hide();
-	    $('ul', $(this)).show();
+	    clicked = true;
+	    $('nav > ul > li > ul').addClass('hidden');
+	    $('ul', $(this)).removeClass('hidden');
     });
+
+	$(window).scroll(function() {
+		// vars
+		var heightWindow = $(window).height();
+		var position = $(document).scrollTop();
+		var elements = $('a[name]');
+		var current;
+		// reset
+		$('nav a').removeClass('current');
+		if (!clicked) $('nav > ul > li > ul').addClass('hidden');
+		// loop
+		var i = elements.length, l = 0;
+		while (--i >= 0) {
+			if (position > $(elements[i]).offset().top - 150) {
+				current = $(elements[i]);
+				break;
+			}
+		}
+		// assign
+		var currentName = current.attr('name');
+		var a = $('nav li a[href="#' + currentName + '"]');
+		a.parents('ul').removeClass('hidden');
+		a.addClass('current');
+		clicked = false;
+	});
 
 })(this['ns'] = this['ns'] || {});
