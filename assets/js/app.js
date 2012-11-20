1c9b1b890fc15ca3d295f01b643ac2077d52c514
Romuald Quantin <romu@soundstep.com>
2012-11-20 17:15:41 +0000

    ios fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,26 @@
-;(function(ns, $, Modernizr, undefined) {
+;(function(ns, undefined) {
 
-	var clicked = false,
-		event = Modernizr.touch ? 'touchstart' : 'click';
+	var ios = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
 
-    $('nav > ul > li').bind(event, function() {
-	    clicked = true;
-	    $('nav > ul > li > ul').addClass('hidden');
-	    $('ul', $(this)).removeClass('hidden');
-    });
+	var clicked = false;
+	var elements = $('a[name]');
+
+	$('nav > ul > li').click(function() {
+		clicked = true;
+		$('nav > ul > li > ul').addClass('hidden');
+		$('ul', $(this)).removeClass('hidden');
+	});
 
 	$(window).scroll(function() {
 		// vars
+		if ($(window).width() < 960) return;
+
 		var heightWindow = $(window).height();
 		var position = $(document).scrollTop();
-		var elements = $('a[name]');
 		var current;
 		// reset
 		$('nav a').removeClass('current');
 		if (!clicked) $('nav > ul > li > ul').addClass('hidden');
-		// loop
 		var i = elements.length, l = 0;
 		while (--i >= 0) {
 			if (position > $(elements[i]).offset().top - 150) {
@@ -32,6 +34,25 @@
 		a.parents('ul').removeClass('hidden');
 		a.addClass('current');
 		clicked = false;
+		// ios fix
+		// http://stackoverflow.com/questions/11297641/mobile-webkit-reflow-issue
+		if (ios) setTimeout(forceReflow, 0);
 	});
 
-})(this['ns'] = this['ns'] || {}, $, Modernizr);
+	function forceReflow(elem){
+		elem = elem || document.documentElement;
+
+		// force a reflow by increasing size 1px
+		var width = elem.style.width,
+			px = elem.offsetWidth+1;
+
+		elem.style.width = px+'px';
+
+		setTimeout(function(){
+			// undo resize, unfortunately forces another reflow
+			elem.style.width = width;
+			elem = null;
+		}, 0);
+	};
+
+})(this['ns'] = this['ns'] || {});
