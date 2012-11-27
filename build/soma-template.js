059d37377ba844e4aac06c30d6ced860fbe84604
Romuald Quantin <romu@soundstep.com>
2012-11-27 16:32:03 +0000

    v0.0.7 template bootstrap fix

--- build/soma-template.js
+++ build/soma-template.js
@@ -1016,10 +1016,11 @@ if (settings.autocreate) {
 		function(c){var f=function(){/loaded|complete/.test(d.readyState)?c():setTimeout(f,10)};f()}:
 		function(c){d.addEventListener("DOMContentLoaded", c, false)}
 	})(/*@cc_on 1@*/);
-	ready(function() {
-		var child = document.body.firstChild;
+	var parse = function(element) {
+		var child = !element ? document.body.firstChild : element.firstChild;
 		while (child) {
 			if (child.nodeType === 1) {
+				parse(child);
 				var attrValue = child.getAttribute(attributes.template);
 				if (attrValue) {
 					var getFunction = new Function('return ' + attrValue + ';');
@@ -1033,7 +1034,8 @@ if (settings.autocreate) {
 			}
 			child = child.nextSibling;
 		}
-	});
+	};
+	ready(parse);
 }
 function bootstrapTemplate(attrValue, element, func) {
 	var tpl = createTemplate(element);
