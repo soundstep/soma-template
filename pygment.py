f25515d50e51f903eb905069376a0297255ae607
Romuald Quantin <romu@soundstep.com>
2012-12-07 18:30:42 +0000

    site update, events in progress

--- pygment.py
+++ pygment.py
@@ -2,12 +2,20 @@ from pygments import highlight
 from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
-code = """<div data-template="Template">{{name}}</div>
+code = """<div id="target">
+  <button id="ct1" >Click</button>
+  <input id="ct2" type="text" value="type something">
+</div>
 <script>
-  function Template(template, scope, element, node) {
-    scope.name = "john";
-    template.render();
+  var template = soma.template.create(document.getElementById('target'));
+  template.scope.handler = function(event) {
+    alert('The event: "' + event.type + '", has been triggered!')
   }
+  var button = template.getNode(document.getElementById('ct1'));
+  var input = template.getNode(document.getElementById('ct2'));
+  button.addEvent('click', 'handler()');
+  input.addEvent('keypress', 'handler()');
+  template.render();
 </script>
 """
 
