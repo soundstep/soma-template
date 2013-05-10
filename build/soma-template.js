f9df543fb49785d1b004bef2375d4f251af73c5d
Romuald Quantin <romu@soundstep.com>
2013-05-10 18:08:03 +0100

    node.js document update

--- build/soma-template.js
+++ build/soma-template.js
@@ -3,7 +3,7 @@
 	'use strict';
 
 	soma.template = soma.template || {};
-	soma.template.version = '0.1.10';
+	soma.template.version = '0.1.11';
 
 	soma.template.errors = {
 		TEMPLATE_STRING_NO_ELEMENT: 'Error in soma.template, a string template requirement a second parameter: an element target - soma.template.create(\'string\', element)',
@@ -1340,35 +1340,40 @@
 	}
 
 
-	// https://github.com/ded/domready
-	var ready=function(){function l(b){for(k=1;b=a.shift();)b()}var b,a=[],c=!1,d=document,e=d.documentElement,f=e.doScroll,g="DOMContentLoaded",h="addEventListener",i="onreadystatechange",j="readyState",k=/^loade|c/.test(d[j]);return d[h]&&d[h](g,b=function(){d.removeEventListener(g,b,c),l()},c),f&&d.attachEvent(i,b=function(){/^c/.test(d[j])&&(d.detachEvent(i,b),l())}),f?function(b){self!=top?k?b():a.push(b):function(){try{e.doScroll("left")}catch(a){return setTimeout(function(){ready(b)},50)}b()}()}:function(b){k?b():a.push(b)}}();
 
-	if (settings.autocreate && typeof document === 'object') {
-		var parse = function(element) {
-			var child = !element ? document.body : element.firstChild;
-			while (child) {
-				if (child.nodeType === 1) {
-					parse(child);
-					var attrValue = child.getAttribute(attributes.template);
-					if (attrValue) {
-						var getFunction = new Function('return ' + attrValue + ';');
-						try {
-							var f = getFunction();
-							if (isFunction(f)) {
-								soma.template.bootstrap(attrValue, child, f);
-							}
-						} catch(err){}
+
+	var ready;
+	if (typeof document === 'object') {
+		// https://github.com/ded/domready
+		var ready=function(){function l(b){for(k=1;b=a.shift();)b()}var b,a=[],c=!1,d=document,e=d.documentElement,f=e.doScroll,g="DOMContentLoaded",h="addEventListener",i="onreadystatechange",j="readyState",k=/^loade|c/.test(d[j]);return d[h]&&d[h](g,b=function(){d.removeEventListener(g,b,c),l()},c),f&&d.attachEvent(i,b=function(){/^c/.test(d[j])&&(d.detachEvent(i,b),l())}),f?function(b){self!=top?k?b():a.push(b):function(){try{e.doScroll("left")}catch(a){return setTimeout(function(){ready(b)},50)}b()}()}:function(b){k?b():a.push(b)}}();
+		if (settings.autocreate) {
+			var parse = function(element) {
+				var child = !element ? document.body : element.firstChild;
+				while (child) {
+					if (child.nodeType === 1) {
+						parse(child);
+						var attrValue = child.getAttribute(attributes.template);
+						if (attrValue) {
+							var getFunction = new Function('return ' + attrValue + ';');
+							try {
+								var f = getFunction();
+								if (isFunction(f)) {
+									soma.template.bootstrap(attrValue, child, f);
+								}
+							} catch(err){}
+						}
 					}
+					child = child.nextSibling;
 				}
-				child = child.nextSibling;
-			}
-		};
-		ready(parse);
+			};
+			ready(parse);
+		}
 	}
 	function bootstrapTemplate(attrValue, element, func) {
 		var tpl = createTemplate(element);
 		func(tpl, tpl.scope, tpl.element, tpl.node);
 	}
+
 	function createTemplate(source, target) {
 		var element;
 		if (isString(source)) {
