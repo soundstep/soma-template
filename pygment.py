3f251f956e1328a95827f0477cf6a377321c0451
Romuald Quantin <romu@soundstep.com>
2014-12-22 11:01:07 +0000

    Added data-class example.

--- pygment.py
+++ pygment.py
@@ -3,17 +3,12 @@ from pygments.lexers import HtmlDjangoLexer
 from pygments.formatters import HtmlFormatter
 
 code = """<div id="target">
-  <p data-repeat="item in items">This key is: {{$key}}</p>
+  <div data-class='{"red": true}'>A data class switch</div>
+  <div data-class='{"red": false}'>A data class switch</div>
 </div>
 <script>
   var template = soma.template.create(document.getElementById('target'));
-  template.scope.items = {
-    'firstname': 1,
-    'surname': 2,
-    'age': 3,
-  };
   template.render();
-</script>
-"""
+</script>"""
 
 print highlight(code, HtmlDjangoLexer(), HtmlFormatter())
